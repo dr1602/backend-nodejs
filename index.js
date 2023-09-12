6f3eea52e0307f9050c88541ff58a3c6c2f71a93
dr1602 <david.arangelg@gmail.com>
2023-09-12 10:29:43 -0600

    POST: Método para Crear

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
--- routes/products.router.js
+++ routes/products.router.js
@@ -31,4 +31,12 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
 module.exports = router;
