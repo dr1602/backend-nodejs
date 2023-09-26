db6f2e25e95084039da082b2f0eb5c003be92049
dr1602 <david.arangelg@gmail.com>
2023-09-26 11:33:01 -0600

    CORS

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const { required } = require('joi');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500/'];
+const options = {
+  origin: (origin, callback) => {
+  if (whitelist.includes(origin)) {
+    callback(null, true);
+  } else {
+    callback(new Error('no permitido'));
+  }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
